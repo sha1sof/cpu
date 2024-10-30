87668e6874f094e2f30e84cc79934035db35ee2d
ilya shishkin <i.shishkin@makves.ru>
2024-10-30 11:31:20 +0300

    4

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	}()
 
 	for {
-		ctx, cancle := context.WithTimeout(context.Background(), 1*time.Second)
+		ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancle()
 
 		usage, currentProcessUsage, err := a.GetCpuUsage(ctx)
